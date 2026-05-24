07e4ab02f29ea2b70412733d367e54605b039305
Dell <muhammadjunaidmehmood1994@gmail.com>
2026-05-24 06:41:58 -0700

    Update runtime to 0.7.1 and fix folder structure

--- rafixdigital-master/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController; // Controller ko yahan import karna zaroori hai
-
-// Isay hata kar niche wala professional route likhein:
-
-Route::get('/', [HomeController::class, 'index'])->name('home');
